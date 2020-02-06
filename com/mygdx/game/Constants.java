c74f238a702124c248b4056eb6fa77f3d800d75c
koteikatima <koteika.tima@gmail.com>
2020-02-06 16:10:41 -0500

    part2

---
+++ com/mygdx/game/Constants.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+
+public  class Constants {
+    public   static  String language="russian";
+    public static final int width=Gdx.graphics.getWidth();
+    public static final int height= Gdx.graphics.getHeight();
+}
